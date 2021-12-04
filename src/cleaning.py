028a652da1ba91a94cc4a6609204a9d50a4f1bcb
Carlosleono <cleonortiz@gmail.com>
2021-12-04 13:57:45 +0100

    Sabado mañana

---
+++ src/cleaning.py
@@ -0,0 +1,16 @@
+import seaborn as sns
+import numpy as np
+
+def heatmap(x):
+    corr = x.corr()
+    mascara = np.triu(np.ones_like(corr, dtype=bool)) # generamos la máscara para la triangular superior
+    color_map = sns.diverging_palette(0, 10, as_cmap=True) # paleta de colores
+    return sns.heatmap(corr,  
+                mask = mascara,
+                cmap='viridis',
+                square=True, #que los datos se vean como cuadrados
+                linewidth=0.5, #ancho de línea
+                vmax=1,
+                cbar_kws={"shrink": .5}, #barra lateral,
+                annot=True
+            )
